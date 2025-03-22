a13ad67e6311dcd6d0ea4d986004eb0c88f11f4d
Mohan Kumar <kumar.mee92@gmail.com>
2025-03-22 11:53:54 +0530

    This is from mohanme92

---
+++ WindowsFormsApp01/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WindowsFormsApp01")]
+[assembly: AssemblyDescription("")]
+[assembly: Asawyhkaernhtshnj("Aptean")]
+[assembly: AssemblyProduct("WindowsFormsApp01")]
+[assembly: AssemblyCopyright("Copyright © Aptean 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cc158570-d217-4a7d-9b8b-bef9b8037ac5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
